5e7d104eea790bac2d4fc4a4d1ab2974e5cdf142
diminator <dimi.dejonghe@gmail.com>
2016-10-13 16:56:17 +0200

    bump to version 0.5.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ docs_require = [
 
 setup(
     name='cryptoconditions',
-    version='0.4.1',
+    version='0.5.0',
     description='Multi-algorithm, multi-level, multi-signature format for '
                 'expressing conditions and fulfillments according to the Interledger Protocol (ILP).',
     long_description=__doc__,
