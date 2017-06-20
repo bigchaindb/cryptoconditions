9f2f46c82b15f3ae99ed4c2a8ed322adb627bcc7
Rodolphe Marques <marques.rodolphe@gmail.com>
2017-06-20 16:55:36 +0200

    Added tests for cryptoconditions.__init__
    - Fixed a bug in fulfillment_to_condition

---
+++ tests/test_cryptoconditions.py
@@ -0,0 +1,40 @@
+import pytest
+
+
+def test_validate_condition(minimal_ed25519):
+    from cryptoconditions import validate_condition
+
+    assert validate_condition(minimal_ed25519.condition_uri) is True
+
+
+def test_validate_fulfillment(minimal_ed25519):
+    from cryptoconditions import validate_fulfillment
+
+    assert validate_fulfillment(minimal_ed25519.fulfillment,
+                                minimal_ed25519.condition_uri,
+                                minimal_ed25519.message) is True
+
+
+def test_validate_fulfillment_condition_mismatch(minimal_ed25519,
+                                                 minimal_preimage):
+    from cryptoconditions import validate_fulfillment
+
+    with pytest.raises(Exception):
+        validate_fulfillment(minimal_ed25519.fulfillment,
+                             minimal_preimage.condition_uri)
+
+
+def test_fulfillment_to_condition(minimal_ed25519):
+    from cryptoconditions import fulfillment_to_condition
+
+    assert fulfillment_to_condition(minimal_ed25519.fulfillment) == \
+        minimal_ed25519.condition_uri
+
+
+def test_from_dict(minimal_ed25519):
+    from cryptoconditions import from_dict
+    from cryptoconditions.fulfillment import Fulfillment
+
+    fulfillment = Fulfillment.from_uri(minimal_ed25519.fulfillment)
+    fulfillment_from_dict = from_dict(fulfillment.to_dict())
+    assert fulfillment_from_dict.condition_uri == fulfillment.condition_uri
