0323b2a8b4b871bc826efb120a56f472b973f0bf
Harjyot Singh <harjyot@edjo.io>
2018-11-09 15:43:00 +0000

    Fixed exeption being thrown

--- cryptoconditions/crypto.py
+++ cryptoconditions/crypto.py
@@ -95,7 +95,7 @@ class Ed25519SigningKey(nacl.signing.SigningKey):
     
     @classmethod
     def generate_with_seed(cls, seed):
-        return cls(nacl.signing.SigningKey(seed, 'bytes').encode(encoder=Base58Encoder))
+        return cls(nacl.signing.SigningKey(seed, _get_nacl_encoder('bytes')).encode(encoder=Base58Encoder))
 
 
 class Ed25519VerifyingKey(nacl.signing.VerifyKey):
